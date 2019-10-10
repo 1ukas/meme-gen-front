7f558e25b8ff564561c322c05b5e6b9dec953a8b
Lukas <lukasmilten@gmail.com>
2019-10-10 23:03:53 +0300

    add firebase config as separate js file

---
+++ src/firebase/index.js
@@ -0,0 +1,23 @@
+import firebase from 'firebase/app';
+import 'firebase/storage';
+import 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDBsblmGeyk0PsnJGwcAYD6B5Ikfsn8q3g",
+  authDomain: "meme-gen-7aecd.firebaseapp.com",
+  databaseURL: "https://meme-gen-7aecd.firebaseio.com",
+  projectId: "meme-gen-7aecd",
+  storageBucket: "meme-gen-7aecd.appspot.com",
+  messagingSenderId: "44846162857",
+  appId: "1:44846162857:web:f08814f9a44c7538b45ab8",
+  measurementId: "G-DJXVBJR0L3"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const storage = firebase.storage();
+const auth = firebase.auth();
+
+export {
+  storage, auth, firebase as default
+}
