e404562b60bc068eca56891db813337c229359a3
Lukas <lukasmilten@gmail.com>
2019-10-10 23:04:58 +0300

    fix className declaration

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,10 +3,10 @@ import React, { Component } from 'react';
 class Footer extends Component {
 	render() {
 		return (
-			<footer className="site-footer my-2 justify-content-center text-center">
+			<footer className="site-footer my-3 justify-content-center text-center">
 				<section>
 					<p>Meme generator by <a href="https://github.com/fanta232">fanta232</a></p>
-					<p>Open-sourced on <a href="https://github.com/fanta232/meme-gen-front"><i class="fab fa-github"></i></a></p>
+					<p>Open-sourced on <a href="https://github.com/fanta232/meme-gen-front"><i className="fab fa-github"></i></a></p>
 				</section>
 			</footer>
 		);
