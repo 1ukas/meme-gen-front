27aa91796a03774837e8c14774049e7258010763
Lukas <lukasmilten@gmail.com>
2019-10-12 00:53:57 +0300

    make onSaveToDevice async / implement onMakePublic

--- src/components/ImageUpload.js
+++ src/components/ImageUpload.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import { save } from 'save-file';
+import uuidv4 from 'uuid/v4';
+import firebase, { storage } from '../firebase';
+
+import '../firebase/auth'; // import anonymous firebase auth
 
 import TextForm from './TextForm';
 
@@ -14,12 +18,36 @@ class ImageUpload extends Component {
 		}
 	}
 
-	onSaveToDevice(name) {
-		save(this.props.imgEdited, name + this.props.imgType);
+	async onSaveToDevice(name) {
+		await save(this.props.imgEdited, name + this.props.imgType);
 	}
 
-	onMakePublic() {
+	onMakePublic(name) {
+    try {
+      const metadata = {
+        customMetadata: { 'memeName': name }
+      }
 
+      const memesRef = storage.ref(`memes/${uuidv4() + this.props.imgType}`).putString(this.props.imgEdited, 'data_url', metadata);
+      memesRef.on(firebase.storage.TaskEvent.STATE_CHANGED, (snapshot) => {
+        switch (snapshot.state) {
+          case firebase.storage.TaskState.PAUSED:
+            console.log('Paused Upload');
+            break;
+          case firebase.storage.TaskState.RUNNING:
+            this.setState({ isLoading: true });
+            console.log('Running Upload');
+            break;
+        }
+      }, (error) => {
+        throw error;
+      }, () => {
+        this.setState({ isLoading: false });
+      });
+    }
+    catch (error) {
+      console.error(error);
+    }
 	}
 
 	render() {
